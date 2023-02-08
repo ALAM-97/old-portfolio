e1f014158c0a48e1ddb5d9fca7c4aa3fa39fb15c
LAPTOP-B44SAJHC\aless <alexamara97@gmail.com>
2023-02-08 10:48:12 +0100

    Update

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,90 +1,98 @@
 <template>
-    <section id="about">
-        <div class="container">
-            <div class="section-intro">
-                <h3>ABOUT ME</h3>
-                <h5>Qualcosa su di me</h5>
+  <section id="about">
+    <div class="container">
+      <div class="section-intro">
+        <h3>ABOUT ME</h3>
+        <h5>Qualcosa su di me</h5>
+      </div>
+      <div class="about-content">
+        <img src="../assets/img/about-logo.png" alt="" />
+        <div class="about-content-right">
+          <p class="about-text">
+            Dopo aver completato il corso Boolean a Dicembre 2022 mi sono subito
+            lanciato nel mercato del lavoro. Ho lavorato i miei primi 3 mesi
+            come web developer in Runtheons, una Peak Performance Company
+            specializzata e focalizzata nell’accelerazione e nel miglioramento
+            delle performance atletiche, e successivamente sono stato assunto in
+            Yeldo, una fintech company che si occupa di investimenti
+            immobiliari, azienda in cui lavoro tutt'ora.
+          </p>
+          <div class="about-numbers">
+            <div>
+              <div class="exp">
+                <strong>2</strong>
+                <p>anni di esperienza</p>
+              </div>
             </div>
-            <div class="about-content">
-                <img src="../assets/img/about-logo.png" alt="">
-                <div class="about-content-right">
-                    <p>
-                        Dopo gli studi ho lavorato per un anno in una start-up che si occupa della creazione di siti internet affiliati al network marketing, nonostante avessi conseguito gli studi da geometra mi ero molto appassionato a quel mondo iniziando a studiare da auto-didatta. Per motivi economici ho dovuto cambiare settore, lavorando per 15 mesi come addetto di scalo presso l'aeroporto internazionale di Roma-Fiumicino. Dopo aver perso il lavoro causa Covid sono tornato sui miei passi e mi sono iscritto al corso Boolean per diventare un Full-Stack Web Developer!
-                    </p>
-                    <div class="about-numbers">
-                        <div>
-                            <div class="exp">
-                                <strong>6</strong>
-                                <p>mesi di esperienza</p>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="exp">
-                                <strong>8+</strong>
-                                <p>progetti completati</p>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="exp">
-                                <strong>0</strong>
-                                <p>lavori in azienda</p>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="btn" href="">
-                        Scarica CV  <i class="fas fa-file-download"></i>
-                    </a>
-                </div>
+            <div>
+              <div class="exp">
+                <strong>10+</strong>
+                <p>progetti completati</p>
+              </div>
             </div>
+            <div>
+              <div class="exp">
+                <strong>2</strong>
+                <p>lavori in azienda</p>
+              </div>
+            </div>
+          </div>
+          <a class="btn" href="">
+            Scarica CV <i class="fas fa-file-download"></i>
+          </a>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    name: 'About',
-}
+  name: "About",
+};
 </script>
 
 <style lang="scss" scoped>
 .about-content {
-    display: grid;
-    grid-template-columns: 1fr 2fr 2.5rem;
-    grid-template-rows: auto;
-    img {
-        justify-self: center;
-        align-self: start;
-        width: 70%;
-        border-radius: 3.125rem;
-    }
-    p {
-        font-size: 1.1875rem;
-    }
-    .btn {
+  display: grid;
+  grid-template-columns: 1fr 2fr 2.5rem;
+  grid-template-rows: auto;
+  img {
+    justify-self: center;
+    align-self: start;
+    width: 70%;
+    border-radius: 3.125rem;
+  }
+  p {
+    font-size: 1.1875rem;
+  }
+  .btn {
     margin: 0 auto;
     align-self: end;
-}
-    .about-content-right {
-        display: grid;
-        grid-template-columns: 1fr;
-        grid-template-rows: 2fr 1fr 4.375rem;
+  }
+  .about-content-right {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 2fr 1fr 4.375rem;
+    .about-text {
+      margin-bottom: 3.4375rem;
     }
-    .about-numbers {
-        display: grid;
-        grid-template-columns:  repeat(3, 1fr);
-        grid-template-rows: auto;
-        align-items: center;
-        .exp {
-            text-align: center;
-            strong {
-                font-size: 1.6875rem;
-            }
-            p {
-                font-size: 1rem;
-                font-weight: 300;
-            }
-        }
+  }
+  .about-numbers {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: auto;
+    align-items: center;
+    .exp {
+      text-align: center;
+      strong {
+        font-size: 1.6875rem;
+      }
+      p {
+        font-size: 1rem;
+        font-weight: 300;
+      }
     }
+  }
 }
-
 </style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,133 +1,147 @@
 <template>
-    <div class="container">
-        <section class="hero">
-            <div class="hero-left">
-                <a class="github-link" href="https://github.com/ALAM-97" target="_blank">
-                    <i class="fab fa-github"></i>
-                </a>
-                <a class="linkedin-link" href="https://www.linkedin.com/in/alessandro-amara-1475a41a1/" target="_blank">
-                    <i class="fab fa-linkedin-in"></i>
-                </a>               
-                <i class="fas fa-print"></i> <!-- cliccare per stampare CV -->         
-            </div>
-            <div class="hero-center">
-                <h1>Ciao, sono Alessandro</h1>
-                <h5>Jr Full-Stack Web Developer</h5>
-                <p>
-                    Mi sono sempre definito una persona creativa con una costante voglia di imparare e crescere, dopo aver lavorato in ambiti che non ho mai sentito 'miei' ho deciso di darmi una possibilità e di cambiare completamente vita e professione, seguendo la mia passione per la tecnologia e l'informatica. A Giugno 2021 mi sono iscritto al corso full-time di Boolean Careers per diventare Web Developer, ora sono alla ricerca di un lavoro in un ambiente che stimoli la mia creatività, che mi permetta di crescere e di interfacciarmi con nuove tecnologie e di mettermi in gioco. 
-                </p>
-            </div>
-            <div class="hero-right">
-            <img src="../assets/img/multicolor-blob.png" alt="">
-            </div>
-        </section>
-    </div>
+  <div class="container">
+    <section class="hero">
+      <div class="hero-left">
+        <a
+          class="github-link"
+          href="https://github.com/ALAM-97"
+          target="_blank"
+        >
+          <i class="fab fa-github"></i>
+        </a>
+        <a
+          class="linkedin-link"
+          href="https://www.linkedin.com/in/alessandro-amara-1475a41a1/"
+          target="_blank"
+        >
+          <i class="fab fa-linkedin-in"></i>
+        </a>
+        <i class="fas fa-print"></i>
+        <!-- cliccare per stampare CV -->
+      </div>
+      <div class="hero-center">
+        <h1>Ciao, sono Alessandro</h1>
+        <h5>Jr Full-Stack Web Developer</h5>
+        <p>
+          Mi sono sempre definito una persona creativa con una costante voglia
+          di imparare e crescere, dopo aver lavorato in ambiti che non ho mai
+          sentito 'miei' ho deciso di darmi una possibilità e di cambiare
+          completamente vita e professione, seguendo la mia passione per la
+          tecnologia e l'informatica. A Giugno 2021 mi sono iscritto al corso
+          full-time di Boolean Careers per diventare Web Developer, ora a
+          distanza 2 anni ho lavorato per due aziende e appreso tanti nuovi
+          concetti e skill.
+        </p>
+      </div>
+      <div class="hero-right">
+        <img src="../assets/img/multicolor-blob.png" alt="" />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Hero',
-}
+  name: "Hero",
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/style/variables.scss';
+@import "../assets/style/variables.scss";
 
 .hero {
+  display: grid;
+  grid-template-columns: 1fr 6fr 6fr;
+  grid-template-rows: auto;
+  width: 100%;
+  margin-top: 3.125rem;
+  column-gap: 4.375rem;
+  .hero-left {
     display: grid;
-    grid-template-columns: 1fr 6fr 6fr;
-    grid-template-rows: auto;
-    width: 100%;
-    margin-top: 3.125rem;
-    column-gap: 4.375rem;
-    .hero-left {
-        display: grid;
-        grid-template-columns: 1fr;
-        grid-template-rows: 2fr 1fr 2fr;
-        grid-template-areas: 
-            "github"
-            "linkedin"
-            "print";
-        i {
-            font-size: 1.5rem;
-            &.fa-github {
-                color: $azureColor;
-            }
-            &.fa-linkedin-in {
-                color: $orangeColor;
-            }
-            &.fa-print {
-                color: $purpleColor;;
-            }
-        }
-        i:hover {
-            opacity: .6;
-        }
-        .github-link {
-            grid-area: github;
-            justify-self: center;
-            align-self: end;
-        }
-        .linkedin-link {
-            grid-area: linkedin;
-            place-self: center;
-        }
-        .fa-print {
-            grid-area: print;
-            justify-self: center;
-        }
+    grid-template-columns: 1fr;
+    grid-template-rows: 2fr 1fr 2fr;
+    grid-template-areas:
+      "github"
+      "linkedin"
+      "print";
+    i {
+      font-size: 1.5rem;
+      &.fa-github {
+        color: $azureColor;
+      }
+      &.fa-linkedin-in {
+        color: $orangeColor;
+      }
+      &.fa-print {
+        color: $purpleColor;
+      }
     }
-    .hero-center {
-        display: grid;
-        grid-template-rows: 6.25rem 4.375rem 2fr;
-        h1 {
-            font-size: 2.5rem;
-            align-self: end;
-        }
-        h5 {
-            font-size: 1.3rem;
-            font-weight: 300;
-            align-self: center;
-        }
-        p {
-            font-size: 1.2rem;
-            align-self: start;
-        }
-        a {
-            margin-right: auto;
-            align-self: start;
-            justify-self: start;
-            margin: 1.25rem 0;
-        }
+    i:hover {
+      opacity: 0.6;
     }
-    .hero-right {
-        display: grid;
-        align-items: center;
-        justify-items: center;
+    .github-link {
+      grid-area: github;
+      justify-self: center;
+      align-self: end;
     }
-    img {
-        width: 90%;
+    .linkedin-link {
+      grid-area: linkedin;
+      place-self: center;
     }
+    .fa-print {
+      grid-area: print;
+      justify-self: center;
+    }
+  }
+  .hero-center {
+    display: grid;
+    grid-template-rows: 6.25rem 4.375rem 2fr;
+    h1 {
+      font-size: 2.5rem;
+      align-self: end;
+    }
+    h5 {
+      font-size: 1.3rem;
+      font-weight: 300;
+      align-self: center;
+    }
+    p {
+      font-size: 1.2rem;
+      align-self: start;
+    }
+    a {
+      margin-right: auto;
+      align-self: start;
+      justify-self: start;
+      margin: 1.25rem 0;
+    }
+  }
+  .hero-right {
+    display: grid;
+    align-items: center;
+    justify-items: center;
+  }
+  img {
+    width: 90%;
+  }
 }
 
 @media (max-width: 1420px) {
-    .show-on-1420 {
-        .hero-center h1 {
-            font-size: 2.1rem;
-            align-self: end;
-        }
-        .hero-center h5 {
-            font-size: 1.1rem;
-        }
-        .hero-center p {
-            font-size: 1.3rem;
-            align-self: start;
-        }
-        .hero-right img {
-            width: 100%;
-        }
+  .show-on-1420 {
+    .hero-center h1 {
+      font-size: 2.1rem;
+      align-self: end;
     }
-    
+    .hero-center h5 {
+      font-size: 1.1rem;
+    }
+    .hero-center p {
+      font-size: 1.3rem;
+      align-self: start;
+    }
+    .hero-right img {
+      width: 100%;
+    }
+  }
 }
-
 </style>
